e53fba1bbf0853d4820c34b193f4c8db3eb224ce
Ricardo Cardona <emy030697@gmail.com>
2023-04-30 15:56:47 -0600

    Add CRMB relay components

--- app/src/main/java/com/example/relay/MainActivity.kt
+++ app/src/main/java/com/example/relay/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.relay
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.relay.crmbbutton.CrmbButton
+import com.example.relay.crmbbutton.State
+import com.example.relay.crmbbutton.Type
 import com.example.relay.hellocard.HelloCard
+import com.example.relay.loyaltypoints.LoyaltyPoints
 import com.example.relay.ui.theme.RelayTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +32,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HelloCard(title = GREETING)
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        HelloCard(title = GREETING)
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        LoyaltyPoints(label = "You have 30 loyalty points!")
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        CrmbButton(
+                            text = "Make a payment",
+                            type = Type.Default,
+                            state = State.Enabled,
+                        )
+                    }
                 }
             }
         }
